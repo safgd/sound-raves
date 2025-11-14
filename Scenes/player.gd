fa55a666c768ba9cff7330781c08dc1b6a6cf668
123amir <safaei.a1@gmail.com>
2025-11-14 04:00:17 +0100

    Fixed healing function and code clean up
    The healing function always healed 1 point, now it's customizable how it was supposed to be.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 @export var max_health: int = 3
-@export var SPEED = 5.0
-@export var JUMP_VELOCITY = 4.5
+@export var speed = 5.0
+@export var jump_velocity = 4.5
 @onready var invulnerability_timer: Timer = $"Invulnerability Timer"
 
 var current_health: int
@@ -26,18 +26,18 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
 	
@@ -51,10 +51,10 @@ func hurt_player(damage: int = 1):
 		get_tree().call_deferred("reload_current_scene")
 
 func heal_player(ammount: int = 1):
-	current_health = clampi(current_health+1, 0, max_health)
+	current_health = clampi(current_health+ammount, 0, max_health)
 
-func _on_area_3d_body_entered(body: Node3D) -> void:
+func _on_shockwave_entered(_body: Node3D) -> void:
 	touching_shockwave = true
 
-func _on_area_3d_body_exited(body: Node3D) -> void:
+func _on_shockwave_exited(_body: Node3D) -> void:
 	touching_shockwave = false
