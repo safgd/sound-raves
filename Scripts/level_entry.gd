c90bd4cc239070184722515ae13329a20b2025ca
123amir <safaei.a1@gmail.com>
2025-11-26 22:40:37 +0100

    Level Entries are now 3d models, representing dance clubs. They can be accessable or not.
    A bouncer stands in front of the doorway. His color and the club's light's color can be customized.

--- Scripts/level_entry.gd
+++ Scripts/level_entry.gd
@@ -1,17 +1,42 @@
 @tool
 extends Area3D
 
+@export var open: bool = false:
+	set(value):
+		open = value
+		if open:
+			$Sprite3D.position = $"Bouncer 2nd Pos".position
+		else:
+			$Sprite3D.position = $"Bouncer Door Pos".position
+
 @export var level_scene_path: String
 @export_multiline var level_name: String = "Level":
 	set(value):
 		level_name = value
 		$Label3D.text = level_name
-		
+
+@export var bouncer_image: Texture2D:
+	set(value):
+		bouncer_image = value
+		$Sprite3D.texture = bouncer_image
+
+@export var light_color: Color:
+	set(value):
+		light_color = value
+		$OmniLight3D.light_color = light_color
+
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
+		if open:
 		#get_tree().call_deferred("change_scene_to_file", level_scene_path)
-		SceneLoader.change_to_scene_async(get_parent().get_parent(), level_scene_path)
+			SceneLoader.change_to_scene_async(get_parent().get_parent(), level_scene_path)
+		else:
+			print("can't enter")
 		
 
 func _ready() -> void:
 	$Label3D.text = level_name
+	if open:
+		$Sprite3D.position = $"Bouncer 2nd Pos".position
+	else:
+		$Sprite3D.position = $"Bouncer Door Pos".position
