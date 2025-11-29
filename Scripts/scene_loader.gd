b15a27c1026bad56fc9419d2669b2e623a74002e
123amir <safaei.a1@gmail.com>
2025-11-29 18:49:06 +0100

    Added feedback, when the game is loading (when scene changing).
    Doesn't work in web export unfortunately.
    Also added an instance of a shock wave scene in an auto load scene, to compile it's shader at game start and not mid game.

--- Scripts/scene_loader.gd
+++ Scripts/scene_loader.gd
@@ -3,15 +3,32 @@ extends Node
 #var world_hub_scene_path: String
 
 func change_to_scene_async(current_scene, scene_path: String):
+	$"CanvasLayer/Control/Loading Label".visible = true
+	$Timer.start()
+	
 	ResourceLoader.load_threaded_request(scene_path)
 	var status = ResourceLoader.load_threaded_get_status(scene_path)
 	while not status == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED:
 
 		status = ResourceLoader.load_threaded_get_status(scene_path)
 		await get_tree().create_timer(0.1).timeout
-
+	
+	
 	var scene: PackedScene = ResourceLoader.load_threaded_get(scene_path)
 	get_tree().change_scene_to_packed(scene)
 	
+	$"CanvasLayer/Control/Loading Label".visible = false
+	$Timer.stop()
 
 	
+
+
+func _on_timer_timeout() -> void:
+	if $"CanvasLayer/Control/Loading Label".text == "Loading":
+		$"CanvasLayer/Control/Loading Label".text = "Loading."
+	elif $"CanvasLayer/Control/Loading Label".text == "Loading.":
+		$"CanvasLayer/Control/Loading Label".text = "Loading.."
+	elif $"CanvasLayer/Control/Loading Label".text == "Loading..":
+		$"CanvasLayer/Control/Loading Label".text = "Loading..."
+	else:
+		$"CanvasLayer/Control/Loading Label".text = "Loading"
--- Scripts/shock_wave.gd
+++ Scripts/shock_wave.gd
@@ -4,16 +4,22 @@ extends Node3D
 @onready var csg_torus: CSGTorus3D = $CSGTorus
 @export var ring_growth: float = 0.05
 @export var random_colors: Array[Color]
-
+@export var no_time_out: bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	AudioManager.audio_tick.connect(change_color)
 	change_color()
+	
+	# instancing a shockwave, to avoid lag when it's instanced later (shader compilation)
+	if no_time_out:
+		$CSGTorus.use_collision = false
+		$Timer.stop()
+		hide()
 
 func _physics_process(delta: float) -> void:
-	
-	csg_torus.inner_radius += ring_growth * delta
-	csg_torus.outer_radius += ring_growth * delta
+	if not no_time_out:
+		csg_torus.inner_radius += ring_growth * delta
+		csg_torus.outer_radius += ring_growth * delta
 
 func change_color():
 	var color: Color = random_colors[randi() % random_colors.size()]
